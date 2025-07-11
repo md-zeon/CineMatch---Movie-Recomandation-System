893b73609f84aecc4535f0d68377377efdadb0d4
Zeanur Rahaman Zeon <zeonrahaman5870@gmail.com>
2025-07-11 22:55:15 +0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
-// $host = 'localhost';
-// $user = 'root';
-// $pass = '';
-// $dbname = 'cinematch';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'cinematch';
 
 
-$host = 'sql12.freesqldatabase.com';
-$user = 'sql12789451';
-$pass = '3EexUQDjF2';
-$dbname = 'sql12789451';
+// $host = 'sql12.freesqldatabase.com';
+// $user = 'sql12789451';
+// $pass = '3EexUQDjF2';
+// $dbname = 'sql12789451';
 
 $conn = new mysqli($host, $user, $pass, $dbname, 3306);
 
