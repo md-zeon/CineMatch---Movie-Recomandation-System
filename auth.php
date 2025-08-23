79374125b3b73cee9d280d36c5ab46ca262d1289
Joti <miskatulmomotaj01@gmail.com>
2025-08-23 23:29:02 +0600

    Added user log in with password verification and sessions

--- auth.php
+++ auth.php
@@ -22,5 +22,27 @@ if (isset($_POST['register'])) {
     } else {
         die("Error: " . $stmt->error);
     }
+} elseif (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            header("Location: index.php");
+        } else {
+            die("Incorrect password!");
+        }
+    } else {
+        die("User not found!");
+    }
 }
 ?>
